247623edff21b5ec1745c827d82aab8a5b140438
one <ekalashnikov@gmail.com>
2021-10-02 12:25:56 -0400

    created pre and post package install composer events

--- scripts/ComposerEventListener.php
+++ scripts/ComposerEventListener.php
@@ -5,12 +5,12 @@ use Composer\Installer\PackageEvent;
 class ComposerEventListener {
 
 	public static function prePackageInstall(PackageEvent $event){
-		$compser = $event->getComposer();
+		$composer = $event->getComposer();
 		var_dump("Preparing to install packages");
 	}
 
 	public static function postPackageInstall(PackageEvent $event){
-		$compser = $event->getComposer();
+		$composer = $event->getComposer();
 		var_dump("Package install now going to generate lock file");
 	}
 
